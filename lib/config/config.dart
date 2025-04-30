151b2105dae8e54ead95f6b086fa25db43e8a9f2
edeegg <34895877+edeegg@users.noreply.github.com>
2025-04-29 20:11:06 -0400

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,7 +3,7 @@ import 'package:global_repository/global_repository.dart';
 class Config {
   Config._();
   static String bashPath = '${RuntimeEnvir.binPath}/bash';
-  static String packageName = 'com.nightmare.termare';
+  static String packageName = 'com.termux';
   // flutter package名，因为这个会影响assets的路径
   static String flutterPackage = '';
 }

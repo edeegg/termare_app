c23e706bcc4daaf0d68867951c2a48f434483c03
edeegg <34895877+edeegg@users.noreply.github.com>
2025-05-01 04:29:45 -0400

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,7 +3,7 @@ import 'package:global_repository/global_repository.dart';
 class Config {
   Config._();
   static String bashPath = '${RuntimeEnvir.binPath}/bash';
-  static String packageName = 'com.termux';
+  static String packageName = 'com.flutteride.android';
   // flutter package名，因为这个会影响assets的路径
   static String flutterPackage = '';
 }
